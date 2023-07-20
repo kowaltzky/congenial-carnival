1a97a11e6b972a26fba99fa2d2ef9ed9550dff47
Karol <karol_kowalski@wp.pl>
2023-07-20 10:49:14 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,26 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var home = document.getElementById("home");
+    var memberMenu = document.getElementById("memberMenu");
+    var skillsMenu = document.getElementById("skillsMenu");
+    var projectsMenu = document.getElementById("projectsMenu");
+    var contactMenu = document.getElementById("contactMenu");
+    var aboutMenu = document.getElementById("aboutMenu");
+    var homeMenu = document.getElementById("homeMenu");
+    member.style.display = "block";
+    skills.style.display = "none";
+    projects.style.display = "none";
+    contact.style.display = "none";
+    about.style.display = "none";
+    home.style.display = "none";
+    memberMenu.style.backgroundColor = "#f1f1f1";
+    skillsMenu.style.backgroundColor = "transparent";
+    projectsMenu.style.backgroundColor = "transparent";
+    contactMenu.style.backgroundColor = "transparent";
+    aboutMenu.style.backgroundColor = "transparent";
+    homeMenu.style.backgroundColor = "transparent";
+}
